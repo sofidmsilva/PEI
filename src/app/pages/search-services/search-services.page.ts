3db9a7d7627287f5a92714bd90bc6fcfa89e7c03
Paulo Barros <paulofmb@hotmail.com>
2019-12-15 17:49:26 +0000

    localização do user adicionada

--- src/app/pages/search-services/search-services.page.ts
+++ src/app/pages/search-services/search-services.page.ts
@@ -8,6 +8,14 @@ import { fromLonLat } from 'ol/proj';
 import OSM from "ol/source/OSM";
 import View from "ol/View";
 import { ServicespetService } from 'src/app/services/servicespet.service';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import Icon from 'ol/style/Icon';
+import Style from 'ol/style/Style';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import TileLayer from 'ol/layer/Tile';
+import TileJSON from 'ol/source/TileJSON';
 
 
 @Component({
@@ -26,6 +34,10 @@ export class SearchServicesPage implements OnInit {
   public option: string;
   public currentEmail: string;
   public userCoords: number[] = []
+  currentLocation;
+  vectorSource;
+  vectorLayer;
+  rasterLayer;
 
   constructor(private router: Router, private service: ServicespetService, private storage: Storage) {
   }
@@ -41,12 +53,69 @@ export class SearchServicesPage implements OnInit {
   public initializeMap() {
     this.storage.get('currentActiveUser').then((userToken) => {
       this.service.getCoordsLocationOfAUser(userToken).then((resolve) => {
+        // this.currentLocation=new Feature({
+        //   geometry:new Point(fromLonLat([resolve.longitude, resolve.latitude ]))
+        // })
+        // this.currentLocation.setStyle(new Style({
+        //   image:new Icon(({
+        //     color:'#8959A8',
+        //     crossOrigin:'anonymous',
+        //     src: '../../../assets/img/point-png-1.png',
+        //     imgSize:[20,20]
+        //   }))
+        // }));
+
+        // this.vectorSource=new VectorSource({
+        //   features:[this.currentLocation]
+        // })
+
+        // this.vectorLayer=new VectorLayer({
+        //   source:this.vectorSource
+        // })
+
+        // this.rasterLayer=new TileLayer({
+        //  source: new TileJSON({
+        //    url:'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
+        //    crossOrigin:''
+
+        //  })
+        // })
+
+        console.log("latitude :", resolve.latitude)
+        console.log("longitude :", resolve.longitude)
+
+        var marker = new Feature({
+          geometry: new Point(
+            fromLonLat([resolve.longitude,resolve.latitude])
+          ),  
+        });
+
+
+
+        marker.setStyle(new Style({
+            image:new Icon(({
+              color:'#8959A8',
+              crossOrigin:'anonymous',
+              src: 'assets/img/point-png-1.png',
+              imgSize:[500,500],
+              scale: 0.09
+            }))
+          }))
+
+        var vectorSource = new VectorSource({
+          features: [marker]
+        });
+
+        var markerVectorLayer = new VectorLayer({
+          source: vectorSource,
+        });
+        
+
         this.map = new Map({
           target: 'map',
-          layers: [
-            new Tile({
-              source: new OSM()
-            })
+          layers: [new Tile({
+            source: new OSM()
+          })
           ],
           view: new View({
             center: fromLonLat([resolve.longitude, resolve.latitude ]),
@@ -54,6 +123,9 @@ export class SearchServicesPage implements OnInit {
           })
     
         })
+
+        console.log("passou aqui")
+        this.map.addLayer(markerVectorLayer);
      });
 
    })
