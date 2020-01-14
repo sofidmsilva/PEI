85e9f1a9813dbd0d7ef07f1af751d875e0305200
rodrigo <rodrigo58@live.com.pt>
2020-01-14 14:11:45 +0000

    Update chatlist.page.ts

--- src/app/pages/chatlist/chatlist.page.ts
+++ src/app/pages/chatlist/chatlist.page.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, ViewChild  } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { NavController, NavParams } from '@ionic/angular';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFirestore} from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Subscription } from 'rxjs';
 import { RegisterService } from 'src/app/services/register.service';
 import { User } from 'src/app/interfaces/user';
 import { map } from 'rxjs/operators';
-
+import { ServicespetService } from 'src/app/services/servicespet.service';
+import { RequestService } from 'src/app/interfaces/request-service';
 //import { reverse } from 'dns';
 
 @Component({
@@ -19,44 +19,52 @@ import { map } from 'rxjs/operators';
 export class ChatListPage implements OnInit {
   @ViewChild(NavController, { static: false }) navCtrl: NavController;
   @ViewChild(NavParams, { static: false }) navParams: NavParams;
-  @ViewChild(AngularFireDatabase, { static: false }) db: AngularFireDatabase;
   
+  private ServicespetSubscription: Subscription;
   private userSubscription: Subscription;
   private messageSubscription;
   private datauser = new Array<User>();
   public  CurrentUser;
-  private checkService;
+  private servicesPet;
+  private service;
   private teste;
-  
-
+  private servicesCollection : AngularFirestoreCollection<RequestService>;
   documentToDomainObject = _ => {
     const object = _.payload.doc.data();
     object.id = _.payload.doc.id;
     return object;
   }
   constructor(private afs: AngularFirestore, 
-    private authServices: AuthService, private userServices: RegisterService,) {
+    private authServices: AuthService, private userServices: RegisterService,private servicespetServices: ServicespetService,) {
+    this.servicesCollection = this.afs.collection<RequestService>('RequestService');
 
     this.userSubscription = this.userServices.getDataUser(this.authServices.getAuth().currentUser.uid).subscribe(
       data => {
         data[0].dateofbirthday= data[0].dateofbirthday.split('T')[0];
         this.datauser = data;
     });
-
-    
+ 
   }
   ngOnInit() {
     this.CurrentUser = this.authServices.getAuth().currentUser
-    // alterar, só serve pra testar com os mesmos 2 users
-    this.teste = this.getDataservice("bEy0cywDUzWgzp8xANghAdIuOsk2")
-    console.log("---------------")
-    console.log(this.teste)
-    console.log("---------------")
+    this.service = this.getserviceRequest("5s706EAvNbao1SinTloJdDisLaA3")
     
   }
 
-  getDataservice(user) {
-    return "teste"
+  getserviceRequest(user){
+    return this.servicesCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map( a =>{
+          const data = a.payload.doc.data();
+          const id =a.payload.doc.id;
+            return{id, ...data};
+        })
+        .filter(item => (item.from == user));
+      })
+    ).subscribe(result => {
+      this.teste = result;
+     // call methods that work with this.data from here
+    });
   }
 }
   
